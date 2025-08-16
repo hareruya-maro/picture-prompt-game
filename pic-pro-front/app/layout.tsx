fb2b5468cb6be6501e4d37dc68398a2ea011a943
Haruya Nakamura <hareruya.maro@gmail.com>
2025-08-17 02:17:20 +0900

    OGP対応追加

--- pic-pro-front/app/layout.tsx
+++ pic-pro-front/app/layout.tsx
@@ -11,6 +11,32 @@ const mochiyPopOne = Mochiy_Pop_One({
 export const metadata: Metadata = {
   title: "ピクプロ！",
   description: "ひらめきで、お題の絵を完全再現！",
+  openGraph: {
+    type: "website",
+    title: "ピクプロ！",
+    description:
+      "ピクプロは、ひらめきでお題の絵を完全再現するゲームです。友達と一緒に楽しもう！",
+    images: [
+      {
+        url: "/ogp/thumbnail.png", // 🌟 静的画像の指定
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    title: "コピっと！~ピクプロ！~",
+    description:
+      "ピクプロは、ひらめきでお題の絵を完全再現するゲームです。友達と一緒に楽しもう！",
+    site: "https://www.copitto.com/",
+    images: {
+      url: "/ogp/thumbnail.png",
+      type: "image/png",
+      width: 1200,
+      height: 630,
+    },
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({
