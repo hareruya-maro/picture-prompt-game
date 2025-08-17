610a0a37e6b3aa3e35f0207cde6744c11ee2bd53
Haruya Nakamura <hareruya.maro@gmail.com>
2025-08-17 12:43:19 +0900

    LoadingSpinnerを使用するように変更

--- pic-pro-front/src/components/AuthProvider.tsx
+++ pic-pro-front/src/components/AuthProvider.tsx
@@ -2,19 +2,13 @@
 
 import { ReactNode } from "react";
 import { useAuth } from "../hooks/useAuth";
+import LoadingSpinner from "./LoadingSpinner";
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const { user, loading } = useAuth();
 
   if (loading) {
-    return (
-      <div>
-        <ruby>
-          読込中<rt>よみこみちゅう</rt>
-        </ruby>
-        ...
-      </div>
-    ); // Or a proper loading spinner
+    return <LoadingSpinner />; // Or a proper loading spinner
   }
 
   return <>{children}</>;
